74257ea8dd20465c9a48faa458f3bfbbfada1fdb
Adam Beckingham <beckadamtheartist@gmail.com>
2019-10-09 22:37:56 -0600

    try to add seeded randoms

---
+++ trek_noise.py
@@ -0,0 +1,32 @@
+
+import math,random
+
+class TrekNoise:
+	def __init__(self,seed):
+		self.seed=seed
+	def getRandom(self,pos,mod):
+		#I need to use a better algorithm. :|
+		return (self.seed+pos.x+pos.y*256+pos.z*65536)%mod
+
+if __name__=='__main__':
+	from trek_vec3 import Vec3
+	#test some things
+	seed=input("seed?")
+	if not len(seed):
+		seed=random.randint(0,2**64)
+	else:
+		n=0; j=2003
+		for c in seed:
+			n+=j*ord(c); j*=1789
+		seed=n
+	print("Seed:",hex(seed))
+	noise=TrekNoise(seed)
+	pos=Vec3(0,0,0)
+	for z in range(80,83):
+		pos.z=z
+		for y in range(80,83):
+			pos.y=y
+			for x in range(80,83):
+				pos.x=x
+				print(pos.x,pos.y,pos.z,"->",noise.getRandom(pos,0xFFFFF1))
+
