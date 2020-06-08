690ca18170294b5ae22dbc6d9e79477a782e76c7
BuildTools <beckadamtheartist@gmail.com>
2020-06-08 12:24:08 -0600

    Start working on standard generations and compositions

--- generate.py
+++ generate.py
@@ -22,21 +22,37 @@ class Generator:
 			self.seed(random.random())
 		return PlanetoidSystem(self._seed,vec3)
 
+#Earth's radius: 6371 km. mass: 5.9e24 kg.
+
+StandardPlanetList = [
+	{"type": "planet","living": True, "mass": 1e24, "radius": 1e6, "colors":[0x2E,0x18,0x7F]},
+	{"type": "planet","living": False,"mass": 1e20, "radius": 5e5, "colors":[0x21,0x22,0x00]},
+	{"type": "planet","living": False,"mass": 1e21, "radius": 6e5, "colors":[0x41,0x42,0x00]},
+	{"type": "planet","living": False,"mass": 6e23, "radius": 3.4e6, "colors":[0x60,0x81,0x21]},
+	{"type": "planet","living": False,"mass": 1e16, "radius": 1e4, "colors":[0xA0,0xA9,0x8D]}
+]
+
+StandardCompositionMaterials = [
+	"rock","H2O","Deutrium","Tritium","N2O","C2H6","CH4","C3H8","C4H12","FeO","U"
+]
+
+StandardStarList = [
+	{"type": "star","mass": 2e30,"radius":7e8,"colors":[0xE7,0xC3,0xEF]},
+	{"type": "star","mass": 3e30,"radius":3e6,"colors":[0xFF,0xDF,0xBF]},
+	{"type": "star","mass": 1e32,"radius":4e9,"colors":[0x11,0x08,0x14]}
+]
+
 
 def PlanetoidSystem(seed,pos):
 	random.seed(seed)
 	x=float(pos['x']); y=float(pos['y']); z=float(pos['z'])
 	mass = random.randint(1e29,9e32)
-	planets = [{"position":Vec3(x,y,z),"velocity":Vec3(),
-			"mass":mass,"name":"Sol"}]
+	yield random.choice(StandardStarList)+{"position":Vec3(x,y,z),"velocity":Vec3(),"name":"Sol"}
 	rad = random.randint(80,300)*1e19
 	grav = GRAVITATIONAL_CONSTANT*mass
 	for i in range(random.randint(2,10)):
 		x+=random.randint(-6e9,61e8)
 		z+=random.randint(-6e9,61e8)
 		y=random.randint(-1e9,1e9)
-		mass=random.randint(1e24,1e26)
-		planets.append({"position":Vec3(x,y,z),"velocity":Vec3(),
-			"mass":mass,"name":"Planet "+chr(i+0x41)})
-	return planets
+		yield random.choice(StandardPlanetList)+{"position":Vec3(x,y,z),"velocity":Vec3(),"name":"Planet "+chr(i+0x41)}
 
