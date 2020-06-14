b7e00ca0c6e7280cc1e92f5f06bb41028496e81e
BuildTools <beckadamtheartist@gmail.com>
2020-06-13 21:29:27 -0600

    Add a debug thing for now

--- server.py
+++ server.py
@@ -405,6 +405,7 @@ class Client:
 
     def register(self, data):
         user,passw,email = [ToUTF8(a[:a.find(b"\0")]) for a in data[1:].split(b"\0",maxsplit=2)]
+        print(user,passw,email)
         self.log("Registering user:",user)
         passw_md5 = hashlib.md5(bytes(passw,'UTF-8')).hexdigest()  # Generate md5 hash of password
         with open('players/accounts.json', 'r') as accounts_file:
@@ -427,6 +428,7 @@ class Client:
     
     def log_in(self, data):
         user,passw = [ToUTF8(a[:a.find(b"\0")]) for a in data[1:].split(b"\0",maxsplit=1)]
+        print(user,passw)
         self.log("Logging in user:",user)
         if user in BANNED_USERS:
             self.send([ControlCodes["LOGIN"],ResponseCodes['BANNED']])
