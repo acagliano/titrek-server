6dacd585c4c55f45879578f7a22c7c66c9bc6660
Anthony Cagliano <ac@clrhome.org>
2020-11-20 23:23:02 -0500

    Fix indenting in trek_filter.py

--- trek_filter.py
+++ trek_filter.py
@@ -2,9 +2,9 @@
 import os,json,traceback
 
 class TrekFilter:
-  path="filter/"
+    path="filter/"
   
-  def __init__(self,log):
+def __init__(self,log):
     os.makedirs(f"{path}")
     self.log=log  
     offenders=[]
@@ -16,17 +16,17 @@ class TrekFilter:
     {"check":"overthreshold","method":self.overThresh,"failaction":self.BLACKLIST}
     ]
     
-  def packetfilter(self,conn,addr,data):
+def packetfilter(self,conn,addr,data):
     try:
-     for r in rules:
-       if not r["method"] in self.checks:
-        raise Exception(f"Method {r["method"]} not implemented")
-       return = r["method"](self, conn, addr, data)
-       if not return:
-        if not r["failaction"] in self.actions:
-          raise Exception(f"Method {r["failaction"]} not implemented")
-        r["failaction"](self, conn, addr)
-      except:
+        for r in rules:
+            if not r["method"] in self.checks:
+                raise Exception(f'Method {r["method"]} not implemented')
+            return = r["method"](self, conn, addr, data)
+            if not return:
+                if not r["failaction"] in self.actions:
+                    raise Exception(f'Method {r["failaction"]} not implemented')
+                r["failaction"](self, conn, addr)
+    except:
         self.log(traceback.print_exc(limit=None, file=None, chain=True))
       
     
