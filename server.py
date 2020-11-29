8446919df29581614bfcc3c207f0d6951c8635ef
Anthony Cagliano <ac@clrhome.org>
2020-11-28 20:44:19 -0500

    Maybe this will work

--- server.py
+++ server.py
@@ -693,7 +693,7 @@ class Client:
 							else:
 								padded_string=PaddedString("", 9, chr(0))+"\0"
 								odata.extend(list(padded_string)+[0,0,0,0])
-						self.send(bytes([ControlCodes["LOAD_SHIP"]]+odata))
+						self.send(bytes([ControlCodes["LOAD_SHIP"]+odata]))
 					elif data[0]==ControlCodes["NEW_GAME_REQUEST"]:
 						self.create_new_game()
 					elif data[0]==ControlCodes["GET_ENGINE_MAXIMUMS"]:
@@ -717,7 +717,7 @@ class Client:
 		
 	def load_shipmodule(self,m):
 		padded_string=PaddedString(m["Name"], 9, chr(0))+"\0"
-		return list(padded_string)+ [m["techclass"], m["techtype"], m["health"], m["status_flags"]]
+		return list(padded_string)+[m["techclass"], m["techtype"], m["health"], m["status_flags"]]
 		
 	def badpacket(self):
 		try:
