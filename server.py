19b27d32f14e1a708e6de3b06c7f486d1b2f1f88
Anthony <acagliano97@gmail.com>
2020-12-13 18:17:15 +0000

    Hopefully fix logfile flushing

--- server.py
+++ server.py
@@ -170,8 +170,8 @@ class Server:
 			with gzip.open(Config.log_archive, 'ab') as gf:
 				with open(Config.log_file, 'rb') as lf:
 					gf.write(lf.read())
-			sleep(1)
-			os.remove(Config.log_file)
+			sleep(2)
+            open(f'{Config.log_file}', 'w+').close()
 			self.log("Instance logfile flushed to archive.")
 		except:
 			self.elog(traceback.print_exc(limit=None, file=None, chain=True))
