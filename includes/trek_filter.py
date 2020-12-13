6899ca27d1cd44182d1e39f245b02c02e1675116
Anthony Cagliano <ac@clrhome.org>
2020-12-12 21:24:14 -0500

    Add sanity checks to filter

--- includes/trek_filter.py
+++ includes/trek_filter.py
@@ -19,6 +19,9 @@ class TrekFilter:
     version="1.0b"
     status=False
     offenders={}
+    enable_sanity=True
+    special_characters = [bytes(a,'UTF-8') for a in ["/","\\","#","$","%","^","&","*","!","~","`","\"","|"]] + \
+					[bytes([a]) for a in range(1,0x20)] + [bytes([a]) for a in range(0x7F,0xFF)]
     
     def __init__(self,path,log,dlog,hitcount,mode):
         # Filter settings
@@ -62,6 +65,12 @@ class TrekFilter:
             ]
             with open(f'{self.path}filter_rules.json', 'w+') as f:
                 json.dump(self.rules,f)
+        try:
+            with open(f'{self.path}packet_specs.json', 'r') as f:
+                self.packet_specs = json.load(f)
+        except IOError:
+            self.log("Packet specs file missing or invalid. Sanity checks disabled.")
+            self.enable_sanity=False
         try:
             self.log(f'[Filter] {self.mode} mode')
             if self.mode == "normal":
@@ -196,7 +205,32 @@ class TrekFilter:
             return False
             
     def sanity(self, addr, data, trusted):
+        if not self.enable_sanity:
+            self.dlog("[Filter] Sanity checks are disabled, skipping!")                          
+            return False
+        packet_id=str(ord(data[0]))
+        if not packet_id in self.packet_specs:
+            self.dlog(f"[Filter] Packet {packet_id} not in speclist. Skipping!")
+            return False                      
+        packet_segments=bytes(data[1:]).split(b"\0")
+        if not len(packet_segments)==len(self.packet_specs[packet_id]["segments"]):
+            self.log("[Filter] Packet segment count invalid!")
+            return True
+        loop_iter=0
+        for seg in self.packet_specs[packet_id]["segments"]:
+            if seg==False:
+                continue
+            response = getattr(self,seg)(packet_segments[loop_iter])
+            if response:
+                return True 
+            loop_iter+=1                       
         return False
+                                        
+    def special_chars(self, segment):
+        if any([a in bytes(segment, 'UTF-8') for a in TrekFilter.special_characters]):
+			return True
+        else
+            return False                                                         
         
     def threshhold(self, addr, data, trusted):
         ip, port = addr
