54f24fba829d54f1354e149063818e57dcdc267a
Anthony <acagliano97@gmail.com>
2020-12-15 20:53:57 +0000

    Commit fixes that dont work

--- server.py
+++ server.py
@@ -287,8 +287,9 @@ class Server:
 				url="https://discord.com/api/webhooks/788494210734358559/4Y5PH-P_rS-ZQ63-sHpfp2FmXY9rZm114BMMAJQsn6xsQHPOquaYC33tOXiVoZ4Ph6Io"
 			if msgtype==1:
 				url="https://discord.com/api/webhooks/788497355359518790/7c9oPZgG13_yLnywx3h6wZWY6qXMobNvCHB_6Qjb6ZNbXjw9aP993I8jGE5jXE7DK3Lz"
-			jstr = f'\{"content":"{sender}: {msg}"\}'
-			command=f'curl -H "Content-Type: application/json" -X POST -d \'{jstr}\' {url}'
+			json = {"content":f"{sender}: {msg}"}
+            jstr = str(json)
+			command=f"curl -H \"Content-Type: application/json\" -X POST -d '{jstr}' {url}"
 			print(command)
 			os.system(command)
 		except:
