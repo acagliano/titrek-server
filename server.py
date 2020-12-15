e49ec7229252029cac48a9226f7f0758ee099af7
Anthony Cagliano <ac@clrhome.org>
2020-12-15 16:13:18 -0500

    Remove the print of the discord command...it works

--- server.py
+++ server.py
@@ -290,7 +290,6 @@ class Server:
 				url="https://discord.com/api/webhooks/788497355359518790/7c9oPZgG13_yLnywx3h6wZWY6qXMobNvCHB_6Qjb6ZNbXjw9aP993I8jGE5jXE7DK3Lz"
 			jstr="{\"content\":\"'\""+sender+": "+msg+"\"'\"}"
 			command=f"curl -H \"Content-Type: application/json\" -X POST -d '{jstr}' {url}"
-			print(command)
 			os.system(command)
 		except:
 			print(traceback.print_exc(limit=None, file=None, chain=True))
