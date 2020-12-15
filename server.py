3d49b93899f8ecd20fe6a5f158359da0e3ee39db
Anthony Cagliano <ac@clrhome.org>
2020-12-15 17:48:00 -0500

    Add repr to the other routine

--- server.py
+++ server.py
@@ -290,7 +290,7 @@ class Server:
 				url="https://discord.com/api/webhooks/788494210734358559/4Y5PH-P_rS-ZQ63-sHpfp2FmXY9rZm114BMMAJQsn6xsQHPOquaYC33tOXiVoZ4Ph6Io"
 			if msgtype==1:
 				url="https://discord.com/api/webhooks/788497355359518790/7c9oPZgG13_yLnywx3h6wZWY6qXMobNvCHB_6Qjb6ZNbXjw9aP993I8jGE5jXE7DK3Lz"
-			msg=msg.replace("'","\'").replace('"','\"')
+			msg=repr(msg).replace("'","\'").replace('"','\"')
 			jstr="{\"content\":\"'\""+sender+": "+msg+"\"'\"}"
 			command=f"curl -H \"Content-Type: application/json\" -X POST -d '{jstr}' {url}"
 			print(command)
