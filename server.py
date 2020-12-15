a5089889b49a5d388225dff66b462cfaea621770
Anthony Cagliano <ac@clrhome.org>
2020-12-15 15:23:17 -0500

    Hopefully fix key error

--- server.py
+++ server.py
@@ -459,7 +459,7 @@ class Server:
 						ostring+=" "
 					ostring=ostring[:-1]
 					self.log("[Server] "+ostring)
-					Server.discord_out("[Server]",ostring,0)
+					self.discord_out("[Server]",ostring,0)
 					self.broadcast(ostring)	# broadcast to all clients
 				elif line[0]=="stop":
 					self.log("Received stop command.")
