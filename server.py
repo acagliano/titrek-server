c24b92caa8b35cf58100eeebbb32b95c1b242f13
Anthony Cagliano <ac@clrhome.org>
2020-12-15 15:55:45 -0500

    Fix the ' vs "

--- server.py
+++ server.py
@@ -289,7 +289,7 @@ class Server:
 				url="https://discord.com/api/webhooks/788497355359518790/7c9oPZgG13_yLnywx3h6wZWY6qXMobNvCHB_6Qjb6ZNbXjw9aP993I8jGE5jXE7DK3Lz"
 			json = {"content":f"{sender}: {msg}"}
 			jstr = str(json)
-			command=f"curl -H \"Content-Type: application/json\" -X POST -d '{jstr}' {url}"
+			command=f"curl -H \"Content-Type: application/json\" -X POST -d \"{jstr}\" {url}"
 			print(command)
 			os.system(command)
 		except:
