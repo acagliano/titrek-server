fba2716c63186320cf3eadbb7318f834978f72d3
Anthony Cagliano <ac@clrhome.org>
2020-12-18 17:44:29 -0500

    fix indenting, streamline some code.

--- runserver.py
+++ runserver.py
@@ -3,27 +3,33 @@ import importlib
 # may want to set up logging in here
 
 class RunServer:
-  def __init__(self):
-    for file in os.walk("includes")[2]:
-      m = os.path.splitext(file)[0]
-      importlib.import_module(f"includes.{m}")
-    self.instance=Server()
-    
-  def run(self):
-    self.instance.run()
-    
-  def reload(self):
-    for file in os.walk("includes")[2]:
-      m = os.path.splitext(file)[0]
-      importlib.reload(f"includes.{m}")
-    self.old=self.instance
-    self.instance=Server()
-    self.instance.clients=self.old.clients
-    del self.old
-    self.run()
+	def __init__(self):
+		for file in os.walk("includes")[2]:
+			try:
+				m = os.path.splitext(file)[0]
+				importlib.import_module(f"includes.{m}")
+				self.instance=Server()
+			except:
+				print(f"Failed to import module includes.{m}.\n Aborting!")
+				
+	def start(self):
+		self.instance.run()
+		
+	def reload(self):
+		for file in os.walk("includes")[2]:
+			try:
+				m = os.path.splitext(file)[0]
+				importlib.reload(f"includes.{m}")
+				self.old=self.instance
+				self.instance=Server()
+				for client in self.old.clients.keys():
+					self.instance.clients[client]=self.old.clients[client]
+    				del self.old
+    				self.start()
        
     
 if __name__ == '__main__':
 	
 	server = RunServer()
-	server.run()
+	# server.add_loggers()
+	server.start()
