4e127f8ebe0df2686693738f8445a5e8b4f76e81
Anthony Cagliano <ac@clrhome.org>
2020-12-18 20:26:39 -0500

    Proper except abort

--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 import logging, os
 import importlib
 # may want to set up logging in here
+# this will be the new server.py (but new name).
+# RunServer defines a dynamic loader class that is capable of importing/reloading modules and launching server instances
 
 class RunServer:
 	def __init__(self):
@@ -15,17 +17,22 @@ class RunServer:
 	def start(self):
 		self.instance.run()
 		
+	def add_loggers(self):
+		# init logging here, instead of in server
+		
 	def reload(self):
-		for file in os.walk("includes")[2]:
-			try:
+		try:
+			for file in os.walk("includes")[2]:
 				m = os.path.splitext(file)[0]
 				importlib.reload(f"includes.{m}")
-				self.old=self.instance
-				self.instance=Server()
-				for client in self.old.clients.keys():
-					self.instance.clients[client]=self.old.clients[client]
-    				del self.old
-    				self.start()
+			self.old=self.instance
+			self.instance=Server()
+			for client in self.old.clients.keys():
+				self.instance.clients[client]=self.old.clients[client]
+			del self.old
+    			self.start()
+		except:
+			print("An error occured")
        
     
 if __name__ == '__main__':
