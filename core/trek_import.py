80bbb1455ccab6400f956606b82aec01f8ca7fe5
Anthony Cagliano <ac@clrhome.org>
2020-12-21 13:23:10 -0500

    Add Trek importer class

--- core/trek_import.py
+++ core/trek_import.py
@@ -1,8 +1,15 @@
-import importlib
+import importlib,subproccess
 
 class TrekImporter:
-  def __call__(self, module, log):
-    try:
-				self.log(logging.INFO, f"Importing module {module}")
+	def __call__(self, module):
+		try:
+			importlib.import_module(module)
+			return True
+		except ImportError:
+			try:
+				subprocess.check_call([sys.executable, "-m", "pip", "install", module])
 				importlib.import_module(module)
+				return True
+			except CalledProcessError:
+				return False
    
