16c6b40ad8080df06dc58d220c14007e3b0994ad
Anthony Cagliano <ac@clrhome.org>
2020-12-21 23:37:00 -0500

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -2,10 +2,10 @@ import os, subprocess, traceback, importlib
 # may want to set up logging in here
 # this will be the new server.py (but new name).
 # RunServer defines a dynamic loader class that is capable of importing/reloading modules and launching server instances
-EXT_LIBS_USED=["discord"]			
-
+EXT_LIBS_USED=["discord"]
 
 class RunServer:
+	count=0
 	def __init__(self):
 		
 		self.import_from_dir("core")		# Import core components of server
@@ -15,12 +15,32 @@ class RunServer:
 	def setup_loggers(self):
 		self.logger=TrekLogging(("logs/server.log", "logs/error.log"))
 		
+	def start_server(self):
+		self.server=Server()
+		self.server.run(self.config)
 		
-	def import_from_dir(self, path, reqd=True):
+	def stop_server(self, number):
+		# implement when multi-instance is a thing
+		
+	def console_emit(self, number):
+		# function where you can attach to the server number of the server you want to look in on
+		
+	def reload_server(self):
+		self.import_from_dir("core", True, True)
+		self.import_from_dir("plugins", False, True)
+		clients=self.server.clients
+		self.server=Server()
+		self.server.clients=clients
+		self.server.run(self.config)
+		
+	def import_from_dir(self, path, reqd=True, reload=False):
 		for file in os.walk(path)[2]:
 			try:
 				lib=os.path.splitext(file)[0]
-				importlib.import_module(f"{path}.{lib}")
+				if reload==True:
+					importlib.reload(f"{path}.{lib}")
+				elif reload==False:
+					importlib.import_module(f"{path}.{lib}")
 			except ImportError:
 				if reqd:
 					sys.exit("Error loading core server component. Aborting!")
@@ -38,8 +58,6 @@ class RunServer:
 				except:
 					pass
 				
-	def start(self):
-		self.server.run(self.config)
 		
        
     
