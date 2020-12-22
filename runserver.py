5ee662f9382948d894ac42d96a576a2373d38e0a
Anthony Cagliano <ac@clrhome.org>
2020-12-21 23:07:40 -0500

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -2,48 +2,44 @@ import os, subprocess, traceback, importlib
 # may want to set up logging in here
 # this will be the new server.py (but new name).
 # RunServer defines a dynamic loader class that is capable of importing/reloading modules and launching server instances
-EXT_LIBS_USED=["discord"]
-
-for lib in os.walk("core")[2]:
-	try:
-		m=os.path.splitext(lib)[0]
-		importlib.import_module(f"core.{m}")
-	except ImportError:
-		sys.exit("Error loading core server component. Aborting!")
-		
-for lib in EXT_LIBS_USED:
-	try:
-		importlib.import_module(lib)
-	except ImportError:
-		try:
-			subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
-			importlib.import_module(lib)
-		except:
-			pass
-			# we will test for this actually not working later on
-			
+EXT_LIBS_USED=["discord"]			
 
 
 class RunServer:
 	def __init__(self):
 		
-		# initialize loggers
+		self.import_from_dir("core")		# Import core components of server
+		self.import_helpers()			# Import (or install/import) helpers for server
+		self.import_from_dir("plugins", False, False)	# Import plugins
+		
+	def setup_loggers(self):
 		self.logger=TrekLogging(("logs/server.log", "logs/error.log"))
-		# attempt to import/install any non-system libraries
+		# import plugins
 		
-		# move imports of addon modules to TrekImporter. We'll invoke installations as needed
-					
-		# attempt to import custom class "modules" the server needs
-		for file in os.walk("includes")[2]:
+	def import_from_dir(self, path, reqd=True):
+		for file in os.walk(path)[2]:
 			try:
-				m=os.path.splitext(file)[0]
-				importlib.import_module(f"includes.{m}")
-				self.server=Server()
-				self.log("Server successfully instantialized"!)
+				lib=os.path.splitext(file)[0]
+				importlib.import_module(f"{path}.{lib}")
 			except ImportError:
-				self.elog(f"Failed to import module 'includes.{m}'. Aborting!")
-				sys.exit(2)
-		
+				if reqd:
+					sys.exit("Error loading core server component. Aborting!")
+				else:
+					pass
+				
+	def import_helpers(self, arr=EXT_LIBS_USED):
+		for lib in arr:
+			try:
+				importlib.import_module(lib)
+			except ImportError:
+				try:
+					subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
+					importlib.import_module(lib)
+				except:
+					if reqd:
+						sys.exit("Error loading core server component. Aborting!")
+					else:
+						pass
 		
 		
 	
@@ -52,20 +48,6 @@ class RunServer:
 		self.config=Config(self.logger)
 		self.server.run(self.config)
 		
-		
-	def reload(self):
-		try:
-			for file in os.walk("includes")[2]:
-				m = os.path.splitext(file)[0]
-				importlib.reload(f"includes.{m}")
-			self.old=self.server
-			self.server=Server()
-			for client in self.old.clients.keys():
-				self.server.clients[client]=self.old.clients[client]
-			del self.old
-    			self.start()
-		except:
-			print("An error occured")
        
     
 if __name__ == '__main__':
