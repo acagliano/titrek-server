907d513f4ceffc8299a9f35b768407d080e7d010
Anthony Cagliano <ac@clrhome.org>
2020-12-21 22:13:44 -0500

    Update trek_discord.py

--- core/trek_discord.py
+++ core/trek_discord.py
@@ -1,7 +1,12 @@
-import discord
+import discord, logging
 
 class TrekDiscord(discord.Client):
   def setlog(log):
+    self.logger=log
     
   async def on_ready(self):
-    print('TrekDiscord has initialized {0}!'.format(self.user))
+    self.logger.log(logging.INFO, "TrekDiscord has successfully initialized!")
+    
+ async def on_message(self, message):
+        # broadcast or PM this, plus log to console (that part's easy)
+        self.logger.log(logging.INFO, "{0.author}: {0.content}'.format(message))
