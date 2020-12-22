b0f090746a8d3380e3534c57ee3f0a3b53fa12a9
Anthony Cagliano <ac@clrhome.org>
2020-12-21 19:10:58 -0500

    Update trek_config.py

--- core/trek_config.py
+++ core/trek_config.py
@@ -14,40 +14,38 @@ class DiscordError(Handler):
 		# send out post request using module of choice
 
 class Config:
-	settings={}
-	logger=False
 	textbody_controlcodes = [ControlCodes["REGISTER"],ControlCodes["LOGIN"],ControlCodes["PING"],ControlCodes["MESSAGE"],\
 						ControlCodes["DEBUG"],ControlCodes["SERVINFO"]]	
 	def init(self, log):
 		try:
-			Config.logger=log
+			self.logger=log
 			with open(f'config.json', 'r') as f:
-				Config.settings=json.load(f)
-				Config.settings["packet-size"]=max(4096, Config.settings["packet-size"])
+				self.settings=json.load(f)
+				self.settings["packet-size"]=max(4096, self.settings["packet-size"])
 				Config.settings["gamedata"]="data/"
 				if not SUPPORTS_SSL:
-					Config.settings["ssl"]["enable"]=False
-				if Config.settings["ssl"]["enable"]:
+					self.settings["ssl"]["enable"]=False
+				if self.settings["ssl"]["enable"]:
 					ssl_path=Config.settings["ssl"]["path"]
-					Config.ssl = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-					Config.ssl.load_cert_chain(f'{ssl_path}/fullchain.pem', f'{ssl_path}/privkey.pem')
-				if Config.settings["enable-discord-link"]:
+					self.ssl = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+					self.ssl.load_cert_chain(f'{ssl_path}/fullchain.pem', f'{ssl_path}/privkey.pem')
+				if self.settings["enable-discord-link"]:
 					if TrekImporter.__call__("discord"):
 						formatter = logging.Formatter('%(levelname)s: %(asctime)s: %(message)s')
 						discord_main=DiscordMain()
 						discord_main.setFormatter(formatter)
 						discord_main.setLevel(logging.INFO)
-						Config.logger.addHandler(discord_main)
+						self.logger.addHandler(discord_main)
 						
 						discord_error=DiscordError()
-						discord_main.setFormatter(formatter)
-						discord_main.setLevel(logging.ERROR)
-						Config.logger.addHandler(discord_error)
+						discord_error.setFormatter(formatter)
+						discord_error.setLevel(logging.ERROR)
+						self.logger.addHandler(discord_error)
 					else:
-						Config.logger.log(logging.ERROR, "Error initializing DiscordHandler()"
-				Config.logger.log(logging.INFO, "Server config loaded!")
+						self.logger.log(logging.ERROR, "Error initializing DiscordHandler()"
+				self.logger.log(logging.INFO, "Server config loaded!")
 		except:
-			self.elog(traceback.format_exc(limit=None, chain=True))
+			self.logger.log(logging.ERROR, traceback.format_exc(limit=None, chain=True))
 	
 	def save(self):
 		try:
@@ -56,5 +54,5 @@ class Config:
 				self.log("Server config written!")
 			return True
 		except:
-			self.elog(traceback.format_exc(limit=None, chain=True))
+			self.logger.log(logging.ERROR, traceback.format_exc(limit=None, chain=True))
 			return False
