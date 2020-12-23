38ed8a17bbcdabebc9c95c4fb55b296bc360417c
Anthony Cagliano <ac@clrhome.org>
2020-12-23 14:44:31 -0500

    fix indent errors in this file

--- core/utils/trek_modules.py
+++ core/utils/trek_modules.py
@@ -1,16 +1,16 @@
 import traceback,logging,os,json
 
 class TrekModules:
-  def __init__(self,path):
-    self.path=path
-    
-  def load_module(self, name, level):
-    try:
-      module_path=f"{self.path}{name}.py"
-		  with open(module_path) as f:
-        modules_data=json.load(f)
-      return modules_data["module"][level]
-    except:
-		  return None
+	def __init__(self,path):
+		self.path=path
+	
+	def load_module(self, name, level):
+		try:
+			module_path=f"{self.path}{name}.py"
+			with open(module_path) as f:
+				modules_data=json.load(f)
+			return modules_data["module"][level]
+		except:
+			return None
     
   
