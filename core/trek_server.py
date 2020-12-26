635fc74db1b0ff7c9aca60bfd7f3f5c8e35c1b4e
Anthony Cagliano <ac@clrhome.org>
2020-12-26 14:37:58 -0500

    Update trek_server.py

--- core/trek_server.py
+++ core/trek_server.py
@@ -84,6 +84,7 @@ class Server:
 			self.main_thread = threading.Thread(target=self.main)
 			self.main_thread.start()
 			self.log(f"Server running on port {self.config.settings['port']}")
+			self.console_emit()
 		#	self.flush_log_to_archive()
 		except:
 			self.elog(traceback.format_exc(limit=None, chain=True))
