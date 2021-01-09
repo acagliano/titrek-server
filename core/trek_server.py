64f0b78cc2d4f09222e9acdb6be00bbf38f9cc7e
Anthony Cagliano <ac@clrhome.org>
2021-01-08 23:08:16 -0500

    Fix say errors

--- core/trek_server.py
+++ core/trek_server.py
@@ -177,7 +177,7 @@ class Server:
 #		self.discord_out(sender,msg,0)
 		for conn in self.clients.keys():
 			client = self.clients[conn]
-			client.send([ControlCodes["MESSAGE"]]+list(bytes(sender+": "+msg+'\0', 'UTF-8')))
+			client.send([ControlCodes["MESSAGE"]]+list(bytes(sender+": "+" ".join(msg)+'\0', 'UTF-8')))
 	
 #	def discord_out(self,sender,msg,msgtype):
 #		if not self.config.settings["enable-discord-link"]:
