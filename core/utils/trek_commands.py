3b9560aee6a807f3c008dab2d337df0643bbf92e
Anthony Cagliano <ac@clrhome.org>
2021-01-08 23:32:18 -0500

    Update trek_commands.py

--- core/utils/trek_commands.py
+++ core/utils/trek_commands.py
@@ -27,23 +27,23 @@ class TrekCommands:
 		try:
 			self.commands={}
 			self.commands["help"]={"permlvl":1, "run":"help", "args":None, "description":"lists all available commands","helper":""}
-			self.commands["reload"]={"permlvl":2, "run":"reload", "args":0, "description":"fully reloads the selected server","helper":"reload"}
+			self.commands["reload"]={"permlvl":2, "run":"reload", "args":False, "description":"fully reloads the selected server","helper":"reload"}
 			self.commands["broadcast"]={"permlvl":1, "run":"broadcast", "args":None, "description":"sends messages to all clients", "helper":"broadcast <msg>"}
 			self.commands["say"]=self.commands["broadcast"]
-			self.commands["stop"]={"permlvl":2, "run":"stop", "args":0, "description":"stops the server", "helper":"stop"}
-			self.commands["save"]={"permlvl":2, "run":"save", "args":0, "description":"saves the map", "helper":"save"}
-			self.commands["seed"]={"permlvl":2, "run":"generator.seed_string", "args":1, "description":"reseeds the generator", "helper":"seed <string>"}
-			self.commands["generate"]={"permlvl":2, "run":"generator.generate_all", "args":0, "description":"reseeds the generator", "helper":"generate"}
-			self.commands["kick"]={"permlvl":1, "run":"kick", "args":1, "description":"kicks the player from the server", "helper":"kick <username|ip>"}
-			self.commands["ban"]={"permlvl":1, "run":"ban", "args":2, "description":"bans the player from the server", "helper":"ban <username|ip>"}
-			self.commands["banlist"]={"permlvl":1, "run":"banlist", "args":0, "description":"shows the list of banned users/ips", "helper":"banlist"}
-			self.commands["fwinfo"]={"permlvl":1, "run":"fw_printinfo", "args":0, "description":"shows firewall status", "helper":"fwinfo"}
-			self.commands["backup"]={"permlvl":2, "run":"backup", "args":0, "description":"backs up entire server state to timestamped file","helper":"backup"}
-			self.commands["restore"]={"permlvl":2, "run":"restore", "args":1, "description":"restores server state from timestamped file","helper":"backup <file>"}
-			self.commands["list"]={"permlvl":0, "run":"list", "args":0, "description":"lists all connected sessions","helper":"list"}
-			self.commands["debug"]={"permlvl":1, "run":"debug", "args":1, "description":"enables/disables server debug mode","helper":"debug enable|disable"}
-			self.commands["discord"]={"permlvl":2, "run":"discord", "args":1, "description":"enables/disables discord error/firewall piping","helper":"discord enable|disable"}
-			self.commands["except"]={"permlvl":1, "run":"trigger_exception", "args":0, "description":"causes a harmless testing exception","helper":"except"}
+			self.commands["stop"]={"permlvl":2, "run":"stop", "args":False, "description":"stops the server", "helper":"stop"}
+			self.commands["save"]={"permlvl":2, "run":"save", "args":False, "description":"saves the map", "helper":"save"}
+			self.commands["seed"]={"permlvl":2, "run":"generator.seed_string", "args":True, "description":"reseeds the generator", "helper":"seed <string>"}
+			self.commands["generate"]={"permlvl":2, "run":"generator.generate_all", "args":False, "description":"reseeds the generator", "helper":"generate"}
+			self.commands["kick"]={"permlvl":1, "run":"kick", "args":True, "description":"kicks the player from the server", "helper":"kick <username|ip>"}
+			self.commands["ban"]={"permlvl":1, "run":"ban", "args":True, "description":"bans the player from the server", "helper":"ban <username|ip>"}
+			self.commands["banlist"]={"permlvl":1, "run":"banlist", "args":False, "description":"shows the list of banned users/ips", "helper":"banlist"}
+			self.commands["fwinfo"]={"permlvl":1, "run":"fw_printinfo", "args":False, "description":"shows firewall status", "helper":"fwinfo"}
+			self.commands["backup"]={"permlvl":2, "run":"backup", "args":False, "description":"backs up entire server state to timestamped file","helper":"backup"}
+			self.commands["restore"]={"permlvl":2, "run":"restore", "args":True, "description":"restores server state from timestamped file","helper":"backup <file>"}
+			self.commands["list"]={"permlvl":0, "run":"list", "args":False, "description":"lists all connected sessions","helper":"list"}
+			self.commands["debug"]={"permlvl":1, "run":"debug", "args":True, "description":"enables/disables server debug mode","helper":"debug enable|disable"}
+			self.commands["discord"]={"permlvl":2, "run":"discord", "args":True, "description":"enables/disables discord error/firewall piping","helper":"discord enable|disable"}
+			self.commands["except"]={"permlvl":1, "run":"trigger_exception", "args":False, "description":"causes a harmless testing exception","helper":"except"}
  
 		except: print(traceback.format_exc(limit=None, chain=True))
 				
@@ -65,8 +65,8 @@ class TrekCommands:
 			self.logger.log(logging.INFO, f"Invalid command usage. {spec['helper']}")
 			return
 		try:
-			if args>0:
-				getattr(self,spec["run"])(commands[1:])
+			if args:
+				getattr(self,spec["run"])(commands[1])
 			else:
 				getattr(self,spec["run"])()
 		except AttributeError: 
