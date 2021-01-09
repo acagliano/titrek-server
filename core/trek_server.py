906c154109ef0caa21ba61eb84815b1958c03c21
Anthony Cagliano <ac@clrhome.org>
2021-01-08 23:20:19 -0500

    Update trek_server.py

--- core/trek_server.py
+++ core/trek_server.py
@@ -175,10 +175,11 @@ class Server:
 		
 	def broadcast(self,msg,sender="Server"):
 #		self.discord_out(sender,msg,0)
-		msg=" ".join(msg)
+		delim=" "
+		text=delim.join(msg)
 		for conn in self.clients.keys():
 			client = self.clients[conn]
-			client.send([ControlCodes["MESSAGE"]]+list(bytes(sender+": "+msg+'\0', 'UTF-8')))
+			client.send([ControlCodes["MESSAGE"]]+list(bytes(sender+": "+text+'\0', 'UTF-8')))
 	
 #	def discord_out(self,sender,msg,msgtype):
 #		if not self.config.settings["enable-discord-link"]:
