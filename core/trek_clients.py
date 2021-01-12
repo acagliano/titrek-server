06cc4169e6ac663c8fe932c9bb2e1b19bc3e81d1
Anthony Cagliano <ac@clrhome.org>
2021-01-11 23:47:45 -0500

    Update trek_clients.py

--- core/trek_clients.py
+++ core/trek_clients.py
@@ -484,7 +484,7 @@ outputs:
 	def kick(self):
 		# send kick to client
 		self.connected=False
-		self.conn.close()
+		self.conn.shutdown(socket.SHUT_WR)
 				    
 	def version_check(self, data):
 		client_version = data[1:4]
