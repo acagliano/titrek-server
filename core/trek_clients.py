905370293b490cb11c96968a023f5914f07c3c84
Anthony Cagliano <ac@clrhome.org>
2021-01-11 23:23:16 -0500

    Update trek_clients.py

--- core/trek_clients.py
+++ core/trek_clients.py
@@ -116,8 +116,8 @@ class Client:
 			
 	def handle_connection(self):
 		self.conn.settimeout(self.config.settings["idle-timeout"])
-		while self.server.online and self.connected:
-			try:
+		try:
+			while self.server.online and self.connected:
 				data = list(self.conn.recv(self.config.settings["packet-size"]))
 				self.fw.filter(self.conn, self.addr, data, self.logged_in)
 				if not data:
@@ -297,19 +297,19 @@ class Client:
 					packet_string = "".join([s.ljust(5," ") for s in [chr(c) if c in range(0x20,0x80) else "0x0"+hex(c)[2] if c<0x10 else hex(c) for c in data]])
 					self.dlog(f"recieved packet: {packet_string}")
 				
-			except socket.timeout:
-				raise ClientDisconnectErr(f"Inactive timeout for user {self.user}. Disconnecting.")
-				break	
-			except socket.error:
-				pass
-			except ClientDisconnectErr as e:
-				self.log(str(e))
-				self.disconnect()
-				self.server.purgeclient(self.conn)
-				self.broadcast(f"{self.user} disconnected")
-				break
-			except Exception as e:
-				self.elog(traceback.format_exc(limit=None, chain=True))
+		except socket.timeout:
+			raise ClientDisconnectErr(f"Inactive timeout for user {self.user}. Disconnecting.")
+			break	
+		except socket.error:
+			pass
+		except ClientDisconnectErr as e:
+			self.log(str(e))
+			self.disconnect()
+			self.server.purgeclient(self.conn)
+			self.broadcast(f"{self.user} disconnected")
+			break
+		except Exception as e:
+			self.elog(traceback.format_exc(limit=None, chain=True))
 				    
 		
 	def load_shipmodule(self,m):
@@ -481,7 +481,11 @@ outputs:
 					return
 		except:
 			self.elog(traceback.format_exc(limit=None, chain=True))
-			
+		
+	def kick(self):
+		# send kick to client
+		self.connected=False
+				    
 	def version_check(self, data):
 		client_version = data[1:4]
 		gfx_version = data[4:6] # not used yet
