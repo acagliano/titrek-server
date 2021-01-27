4da468166161a2772c88dc64bf4f47484a9924d8
Anthony Cagliano <ac@clrhome.org>
2021-01-27 17:49:21 -0500

    Update logging.py

--- trek/utils/logging.py
+++ trek/utils/logging.py
@@ -89,14 +89,20 @@ class TrekLogging:
 	def log(self, lvl, msg):
 		self.logger.log(lvl, msg)
 
-	def enable_discord(self, exc_thread, filter_thread):
-		if not (exc_thread is None):
+	def enable_discord(self):
+		try:
+			with open("webhooks.json") as f:
+				webhooks=json.load(f)
+		except IOError:
+			return
+		
+		if not (webhooks["exc"] is None):
 			discord_handler_exc=DiscordHandler(exc_thread, logging.ERROR)
 			discord_handler_exc.setFormatter(TrekLogging.formatter)
 			discord_handler_exc.setLevel(logging.ERROR)
 			self.logger.addHandler(discord_handler_exc)
 
-		if not (filter_thread) is None:
+		if not (webhooks["filter"] is None):
 			discord_handler_filter=DiscordHandler(filter_thread,logging.FILTER)
 			discord_handler_filter.setFormatter(TrekLogging.formatter)
 			discord_handler_filter.setLevel(logging.FILTER)
