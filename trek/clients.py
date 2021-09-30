bd54f5ac7f8dbba694b90c9e733ab85dddd5adbc
Anthony Cagliano <ac@clrhome.org>
2021-09-30 13:20:53 -0400

    Update clients.py

--- trek/clients.py
+++ trek/clients.py
@@ -406,7 +406,7 @@ outputs:
 		send_size = buf_len
 		if (self.gfx_len - self.gfx_curr) < buf_len:
 			send_size = self.gfx_len - self.gfx_curr
-		self.send([ControlCodes['GFX_FRAME_IN']]+self.gfx_bin[self.gfx_curr:send_size])
+		self.send([ControlCodes['GFX_FRAME_IN']]+list(self.gfx_bin[self.gfx_curr:send_size]))
 		self.gfx_curr += send_size
 
 
