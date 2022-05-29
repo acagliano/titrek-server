a9c2eb46e2bd1399b4f30515b2bf2ddd9911b201
Anthony Cagliano <acagliano97@gmail.com>
2022-05-29 10:30:37 -0400

    Update space2.py

--- trekcore/space2.py
+++ trekcore/space2.py
@@ -1,3 +1,4 @@
+import json,os,sys,logging
 
 class Space:
 	def __init__(self, dir, log, config)
@@ -9,7 +10,22 @@ class Space:
 	def load(self):
 	# this will determine if a map has already been created and load it
 	# or it will create a new map
+		try:
+			self.logger.log(logging.INFO, f"Loading map.")
+			with open(f"{self.path}/universe.meta", "r") as f:
+				universe_meta = json.load(f)
+		galaxies = [ item for item in os.listdir(self.path) if os.path.isdir(os.path.join(root, item)) ]
+		for galaxy in galaxies:
+			self.map.append(CelestialObject(f"{self.path}/{galaxy}"))
+				
+		except IOError:
+			self.logger.log(logging.INFO, f"Universe meta file not found. Generating new map.")
+			self.generate_map()
+		except:
+			self.logger.log(logging.ERROR, f"Universe meta file not found. Loading new map.")
 	
+	
+	def generate_map(self):
 	
 	def tick(self):
 	# this will iterate all tickable stuff, like star life cycles, pathing of objects, etc
