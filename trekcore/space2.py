6c26dd694d33cfa434bdcf1513d41c04c399b2e7
Anthony Cagliano <acagliano97@gmail.com>
2022-05-30 00:13:33 -0400

    Start writing loading code for the map

--- trekcore/space2.py
+++ trekcore/space2.py
@@ -1,28 +1,32 @@
-import json,os,sys,logging
+import json,os,sys,logging,traceback
+
+INIT_MODE_LOAD = 0
+INIT_MODE_GENERATE = 1
 
 class Space:
 	def __init__(self, dir, log, config)
 		self.config=config
 		self.logger=log
 		self.path=dir
+		self.map={}
 		
 		
 	def load(self):
 	# this will determine if a map has already been created and load it
 	# or it will create a new map
 		try:
-			self.logger.log(logging.INFO, f"Loading map.")
 			with open(f"{self.path}/universe.meta", "r") as f:
 				universe_meta = json.load(f)
 			galaxies = [ item for item in os.listdir(self.path) if os.path.isdir(os.path.join(self.path, item)) ]
-			for galaxy in galaxies:
-				self.map.append(CelestialObject(f"{self.path}/{galaxy}"))
+			self.logger.log(logging.INFO, f"Loading map save.")
+			for g in galaxies:
+				self.map.append(Galaxy(f"{self.path}/{g}", INIT_MODE_LOAD))
 				
 		except IOError:
 			self.logger.log(logging.INFO, f"Universe meta file not found. Generating new map.")
 			self.generate_map()
 		except:
-			self.logger.log(logging.ERROR, f"Universe meta file not found. Loading new map.")
+			self.logger.log(logging.ERROR, traceback.print_exc(limit=None, file=None, chain=True))
 	
 	
 	def generate_map(self):
@@ -47,16 +51,43 @@ class Space:
 	
 	
 class CelestialObject:
-	def __init__(self):
+	def __init__(self, filepath, mode):
+		self.path = filepath
+		self.identifier = os.path.basename(filepath).split('.')[0])
+		self.contains = {}
+		if mode==INIT_MODE_LOAD:
+			self.load()
+		elif mode==INIT_MODE_GENERATE:
+			self.generate()
 		
 		
 class Galaxy(CelestialObject):
+	def load(self):
+		systems = [ item for item in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, item)) ]
+		for s in systems:
+			self.contains.append(System(f"{self.path}/{s}", INIT_MODE_LOAD))
+			
+		
+	def generate(self):
+		for x in <num of systems>:
+			
+	def save(self)
 	
 	
 class System(CelestialObject):
+	def load(self):
+		
+	def generate(self):
+		
+	def save(self):
+	
 	
 
-class Planetoid(CelestialObject):
+class SystemBody(CelestialObject):
+	def load(self):
+		
+	def generate(self):
+		
+	def save(self):
 	
-	
-class Stellar(CellestialObject):
+
