093a1cb5f21f3b9b1636988bd8107792dc543b7b
Anthony Cagliano <acagliano97@gmail.com>
2022-07-20 22:15:40 -0400

    Update clients.py

--- trekcore/clients.py
+++ trekcore/clients.py
@@ -265,7 +265,7 @@ outputs:
 				os.system("convimg")
 				with open(tmpfile_search_path,'rb') as f:
 					tosend = f.read())
-			os.remove("convimg.yaml")
+				os.remove("convimg.yaml")
 			return list(tosend.ljust(66, chr(0))
 					  
 				
