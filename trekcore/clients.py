174d536c226543eaf51b9094ae1eec11a80b7668
Anthony Cagliano <acagliano97@gmail.com>
2022-07-28 13:30:12 -0400

    Update clients.py

--- trekcore/clients.py
+++ trekcore/clients.py
@@ -458,7 +458,7 @@ outputs:
 			username = decrypted_data[:delim].decode('ascii')
 			key = decrypted_data[delim+1:]
 			print(f"{len(key)}")
-			sys.stdout.buffer.write(key) 
+			print(key.decode('hex'))
 			
 			#padded_key = credentials[1]
 			#padding = padded_key[len(padded_key)-1]
