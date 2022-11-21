57b7f837352448ad12b187827821464b9246a137
Anthony Cagliano <acagliano97@gmail.com>
2022-11-20 23:19:43 -0500

    commit space.conf file

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import os,random
+import configparser
 
 # Space class for map data
 ######################################
@@ -14,56 +15,40 @@ class Space:
 			self.generate()
 			
 		except IOError:
-			yaml_string = """
-				map-size:
-					start: 0
-					end: ?
-					step: 5
-				scale-distances: 1
-				realistic-physics: false
-				rogue-objects: false
-				galaxies-per-unit:
-					min: 1
-					max: 3
-				systems-per-galaxy: 10
-					min: 10
-					max: 50
+			config_string = """
+				
+				generation-rates:
+					galaxy: "fast"
+					system: "fast"
+				_comments:
+					map-size: "specifies initial size, amount to increase, interval to increase, and size to stop"
+					scale-distances: "Specifies ratio of emulated distance to real distance. 1 means exact scaling"
+					realistic-physics: "Determines whether to path celestial objects, tick star life cycles, compute gravity on dynamic entities, and spawn and path rogue objects like comets"
+					generation-rates: "Sets preconfigured spawn rates of galaxies per Mpc of map space and systems per galaxy. Supported options are 'fast' and 'normal'. Normal attempts to emulate spawning patterns consistent with real life"
+					
 			"""
 			self.config = yaml.safe_load(yaml_string)
 			with open(self.config_file, 'w') as f:
 				yaml.dump(self.config, f)
-			return
+			self.generate()
 		
 		
 	def generate(self):
 		# generate galaxies
 		self.realsize = 0
 		self.galaxies = []
+		galaxies_per = self.config["galaxies-per-Mpc"]
 		if self.config["map-size"] == 0:
 			self.galaxies[0] = galaxy = Galaxy(self.path)
 			galaxy.generate()
 		else:
 			while self.realsize < self.config["map-size"]:
-				galaxies_to_generate = random.choice(range(self.config["min-galaxies-per-unit"], self.config["max-galaxies-per-unit"]))
+				galaxies_to_generate = random.choice(range(galaxies_per["min"], galaxies_per["max"]))
 				for g in galaxies_to_generate:
 					self.galaxies[galaxy_idx] = galaxy = Galaxy(self.path)
-					galaxy.generate()
+					galaxy.generate(self.realsize)
 				
-				
-	def generate_galaxy(self, single_galaxy=False):
-		# spawn galaxy center at random point on circle
-		# defined by a radius of self.realsize,
-		# centered on (0,0,0)
-		# all galaxy origin coordinates are relative to map origin
-		if single_galaxy==True:
-			return CelestialObject("galaxy", 0, 0, 0)
-			
 
-
-CelestialObjectTypes = {
-	"galaxy": 0,
-	"system": 1
-}
 
 class Galaxy(CelestialObject):
 	identifier = 0
@@ -71,16 +56,20 @@ class Galaxy(CelestialObject):
 		self.id = Galaxy.identifier
 		self.path = f"{path}/galaxy{self.id}"
 		Galaxy.identifier += 1
-	def generate(self, dist_from_origin, min_systems, max_systems, single_galaxy=False):
-		# do stuff
+
+
+class System(CelestialObject):
+	identifier = 0
+	def __init__(self, path):
+		self.id = System.identifier
+		self.path = f"{path}/system{id}.dat"
+		System.identifier += 1
 
 
 class CelestialObject:
-	identifier = 0
-	def __init__(self, path):
-		self.id = CelestialObject.identifier
-		self.path = f"{path}/galaxy{self.id}"
-		CelestialObject.identifier += 1
+	def __init__(self):
+		return
 		
+	def generate(self, dist_from_origin):
 		
 		
